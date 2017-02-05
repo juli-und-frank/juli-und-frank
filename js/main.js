f0b804b30e617a859902f130eb0d842e00722dc8
Frank C. Eckert <frank.eckert@boost-project.com>
2017-02-05 17:44:12 +0100

    working form

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
-
 function main() {
 
 (function () {
    'use strict';
 
-	// Hide .navbar first
-	$(".navbar").hide();
-	
-	// Fade in .navbar
-	$(function () {
-		$(window).scroll(function () {
+  // Hide .navbar first
+  $(".navbar").hide();
+
+  // Fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 200) {
-				$('.navbar').fadeIn();
-			} else {
-				$('.navbar').fadeOut();
-			}
-		});
+      if ($(this).scrollTop() > 200) {
+        $('.navbar').fadeIn();
+      } else {
+        $('.navbar').fadeOut();
+      }
+    });
 
-	
-	});
-	
-	// Preloader */
-	  	$(window).load(function() {
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+  });
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
+  // Preloader */
+      $(window).on('load', function() {
 
-  	}) 
+     // will first fade out the loading animation
+      $("#status").fadeOut("slow");
+
+      // will fade out the whole DIV that covers the website.
+      $("#preloader").delay(500).fadeOut("slow").remove();
+
+    });
 
    // Page scroll
-  	$('a.page-scroll').click(function() {
+    $('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -47,7 +46,7 @@ function main() {
       });
 
     // Show Menu on Book
-    $(window).bind('scroll', function() {
+    $(window).on('scroll', function() {
         var navHeight = $(window).height() - 100;
         if ($(window).scrollTop() > navHeight) {
             $('.navbar-default').addClass('on');
@@ -56,23 +55,24 @@ function main() {
         }
     });
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
-    })
+    });
 
-  	$(document).ready(function() {
-  	    $("#testimonial").owlCarousel({
+    /*
+    $(document).ready(function() {
+        $("#testimonial").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem:true
         });
 
-  	});
+    }); */
 
-  	// Portfolio Isotope Filter
-    $(window).load(function() {
+    // Portfolio Isotope Filter
+    $(window).on('load', function() {
         var $container = $('.portfolio-items');
         $container.isotope({
             filter: '*',
@@ -98,23 +98,23 @@ function main() {
         });
 
     });
-	
-	
+
+
 
   // jQuery Parallax
   function initParallax() {
-    $('#intro').parallax("100%", 0.3);
-    $('#services').parallax("100%", 0.3);
-    $('#aboutimg').parallax("100%", 0.3);	
-    $('#testimonials').parallax("100%", 0.1);
+    $('#intro').parallax({ "imageSrc": "img/lovebirds.jpg", "speed": 0.3 });
+    $('#services').parallax({ "speed": 0.3 });
+    $('#aboutimg').parallax({ "speed": 0.3 });
+    $('#testimonials').parallax({ "speed": 0.3 });
 
   }
   initParallax();
 
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+    // Pretty Photo
+  $("a[rel^='prettyPhoto']").prettyPhoto({
+    social_tools: false
+  });
 
 }());
 
